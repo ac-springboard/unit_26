f0465520a2cc9cf351654fe79cf31bc87f13c313
Almir Campos <almircampos@gmail.com>
2021-02-15 12:21:58 -0800

    [U26] P1S2 - Fix Logout - Completed

--- project/warbler/app.py
+++ project/warbler/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, flash, redirect, session, g
+from flask import Flask, render_template, request, flash, redirect, session, g, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
@@ -25,23 +25,24 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
-from csv import DictReader
-from app import db
-from models import User, Message, Follows
 
-db.drop_all()
-db.create_all()
-
-with open('generator/users.csv') as users:
-    db.session.bulk_insert_mappings(User, DictReader(users))
-
-with open('generator/messages.csv') as messages:
-    db.session.bulk_insert_mappings(Message, DictReader(messages))
-
-with open('generator/follows.csv') as follows:
-    db.session.bulk_insert_mappings(Follows, DictReader(follows))
-
-db.session.commit()
+# from csv import DictReader
+# from app import db
+# from models import User, Message, Follows
+#
+# db.drop_all()
+# db.create_all()
+#
+# with open('generator/users.csv') as users:
+#     db.session.bulk_insert_mappings(User, DictReader(users))
+#
+# with open('generator/messages.csv') as messages:
+#     db.session.bulk_insert_mappings(Message, DictReader(messages))
+#
+# with open('generator/follows.csv') as follows:
+#     db.session.bulk_insert_mappings(Follows, DictReader(follows))
+#
+# db.session.commit()
 
 
 ##############################################################################
@@ -133,6 +134,9 @@ def logout():
     """Handle logout of user."""
 
     # IMPLEMENT THIS
+    session.pop(CURR_USER_KEY)
+    flash('User logged out', category='success')
+    return redirect(url_for('login'))
 
 
 ##############################################################################
